a3dea1d4de1610365d19b502b0fdcfc2475e5b0b
Bradley Marranca <account+githubcom@bradmarranca.com>
2024-06-11 11:40:14 -0400

    shi

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::{thread, fs};
+
+fn get_page(path: &str) -> String {
+    return fs::read_to_string("public/".to_owned() + path).expect("Failed to read file");
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("Request: {}", request);
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".to_owned() + &get_page("/index.html");
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => { 
+                eprintln!("Failed: {}", e);
+            }
+        }
+    }
+}
