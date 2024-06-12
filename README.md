2653517a0f31aa2d0140baac2f18a72f833839c4
bradmarr <bradmarranca@gmail.com>
2024-06-11 21:01:41 -0400

    updates to names in docker update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # How to run
 `git clone https://github.com/BradMarr/wings.git`
 
-`docker build -t wings .`
+`docker build -t wings_image .`
 
-`docker run --rm -p 80:80 wings`
+`docker run -d --name wings_container -p 80:80 wings_image`
 
 go to `127.0.0.1`
--- docker_update.sh
+++ docker_update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Stop the running container
-docker stop wings
+docker stop wings_container
 
 # Remove the container
-docker rm wings
+docker rm wings_container
 
 # Remove the old image
-docker rmi wings
+docker rmi wings_image
 
 # Build a new image
-docker build -t wings .
+docker build -t wings_image .
 
 # Run a new container from the new image
-docker run -d --name wings -d -p 80:80 wings
+docker run -d --name wings_container -d -p 80:80 wings_image
